ffe72f9bb7685de5f830be33dcc86483674c749f
Quinn Donovan <qld2@illinois.edu>
2018-04-18 12:19:52 -0500

    Painters Partition almost done.

--- Set-Builder.py
+++ Set-Builder.py
@@ -8,70 +8,19 @@
 
 from sys import argv
 
-k = int(argv[1])
-n = int(argv[2])
+numOfPainters = int(argv[1])
+numOfBoards = int(argv[2])
 
-end = []
-
-for i in range(k - n + 1, k + 1) :
-    end.append(i)
-
-arr = [0] * n
-
-for i in range(0, n) :
-    arr[i] = i
-
-    
-#for i in range(0, k - n + 2) :
-    
-#    for j in range(i + 1, k - n + 3) :
-        
-#        for x in range(j + 1, k - n + 4) :
-        
-#            print([i,j,x])
-
-
-#for i in range(0, k - n + 2) :
-    
-#    for j in range(i + 1, k - n + 3) :
-        
-#        for x in range(j + 1, k - n + 4) :
-        
-#            for y in range(x + 1, k - n + 5) :
-            
-#                print([i,j,x,y])
-
-
-def filler(arr, iterator, t) :
+def filler(arr, iterator, t, n, k) :
     
     if t == n + 3 :
         print(arr)
     else :
         for i in range(iterator + 1, k - n + t) :
             arr[t - 2] = i
-            filler(arr , i, t + 1)
+            filler(arr , i, t + 1, n, k)
                 
    
-filler([0,1,2,3], -1, 2)
-   
-#for t in range(2, 2 + n) :                
-    
-#    a = [] * n           
-    
-#    for i in range(t - 2, k - n + t) :
-    
-#        a.append(i)
-        
-#    print(a)
-    
-#while arr != end :
-#    for i in range(0, n - 1) :
-#    
-#        if arr[i] < arr[i + 1] - 1 :
-#            arr[i] += 1
-#    if arr[n - 1] < k :
-#        arr[n - 1] += 1
-#        
-#    print(arr)
+filler([0] * (numOfPainters - 1), -1, 2, numOfPainters - 2, numOfBoards - 1)
     
     
--- The-Painters-Partition-Problem.py
+++ The-Painters-Partition-Problem.py
@@ -3,44 +3,61 @@
 #https://practice.geeksforgeeks.org/problems/the-painters-partition-problem/0
 
 #code
-T = int(input())
+from sys import argv
+
+T = int(argv[1])
     
 def get_time(boards, partitions) :
     max = 0
-    counter = 0
-    i = 0
+    sum = 0
     j = 0
     
-    while i < len(boards) :
-        
+    for i in range(0, len(boards)) :
         if i == partitions[j] :
-            if counter > max :
-                max = counter
-            counter = 0
-            j += 1
+            if sum > max :
+                max = sum
+            sum = 0
             
-        counter += boards[i]
-        i += 1
-        
+            if j < len(partitions)  - 1 :
+                j += 1
+        sum += boards[i]    
     return max
+    
+possiblePartitions = []    
+    
+def filler(arr, iterator, t, n, k) :
+    
+    if t == n + 3 :
+        possiblePartitions.append(list(arr))
+    else :
+        for i in range(iterator + 1, k - n + t) :
+            arr[t - 2] = i
+            filler(arr , i, t + 1, n, k)
     
 for t in range(0,T) :
 
-    #inp = str(input()).split(' ')
+    inp = str(argv[2]).split(' ')
 
-    #k = int(inp[0])
+    numOfPainters = int(inp[0])
 
-    #n = int(inp[1])
+    numOfBoards = int(inp[1])
 
-    #boards = str(input()).split(' ')
-    #boards = list(map(int, boards))
+    boards = str(argv[3]).split(' ')
+    boards = list(map(int, boards))
     
-    #parts =  [[] * k]
-    partitions = []
-    times = []
+    filler([0] * (numOfPainters - 1), -1, 2, numOfPainters - 2, numOfBoards - 2)
     
-    #print(boards)
-    print(get_time([7, 9, 5, 5, 3], [1,2,4]))
+    min = 0
+    
+    print(get_time(boards, [1, 3]))
+    
+    for i in range(0, len(possiblePartitions)) :
+        #time = get_time(boards, i)
+        #if time < min :
+        #    min = time
+        print(possiblePartitions[i])
+        print(get_time(boards, possiblePartitions[i]))    
+    #print(min)
 
     
 
