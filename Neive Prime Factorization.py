e495e53040570f4c84c26c2abf43487965a248b8
Quinn Donovan <qld2@illinois.edu>
2018-05-13 17:19:29 -0500

    Prime factorizer works

--- Neive Prime Factorization.py
+++ Neive Prime Factorization.py
@@ -1,14 +1,20 @@
-def number = 1240;
-
+from sys import argv
 import math
 
+number = int(argv[1])
+
 def is_prime(x) :
-    for a in range(2, math.sqrt(x)) :
+    for a in range(2, int(math.sqrt(x)) + 1) :
         if x%a == 0 :
             return 0
     return 1
 
-
-for x in range(2, math.sqrt(number)) :
-    if is_prime(x) == 1 :
-        
+def factorize(x) :
+    if is_prime(x) :
+        return str(int(x)) + "."
+    for i in range(2, int(math.sqrt(x)) + 1) :
+        if is_prime(i) == 1 and x%i == 0 :
+            return str(i) + ", " + factorize(x/i)
+    return str()
+    
+print(factorize(number))
